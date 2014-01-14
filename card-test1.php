d30f703b5adeea3b689408a1135722823ded8ea0
chunshin (cygwin) <l7263626@msn.com>
2014-01-14 02:25:43 +0000

    增加轉帳行別

--- card-test1.php
+++ card-test1.php
@@ -1,5 +1,13 @@
 <?php
 require_once "TP/class.TemplatePower.inc.php";
+require_once "conf/vaccount.php";
 $tpl = new TemplatePower("test1.html");
 $tpl->prepare();
+foreach($cms_cfg['vaccount']['Bank'] as $code=>$name){
+    $tpl->newBlock("BANK_LIST");
+    $tpl->assign(array(
+        "BANK_CODE" => $code,
+        "BANK_NAME" => $name,
+    ));
+}
 $tpl->printToScreen();
--- conf/vaccount.php
+++ conf/vaccount.php
@@ -6,5 +6,14 @@ $cms_cfg['vaccount']['params']['BankName'] = 'TAISHIN';  //銀行代碼請參考
 $cms_cfg['vaccount']['params']['ReplyURL'] = $cms_cfg['base_url']."card-test3.php"; //接收授權結果通知網址(以client post方式返回)
 $cms_cfg['vaccount']['Hash']['Key'] = "ejCk326UnaZWKisg";
 $cms_cfg['vaccount']['Hash']['IV'] = "q9jcZX8Ib9LM8wYk";
+$cms_cfg['vaccount']['Bank'] = array(
+    'TAISHIN' => "台新銀行",
+    'HUANAN' => "華南銀行",
+    'ESUN' => "玉山銀行",
+    'FUBON' => "台北富邦銀行",
+    'BOT' => "台灣銀行",
+    'CHINATRUST' => "中國信託",
+    'FIRST' => "第一銀行",
+);
 $cms_cfg['vaccount']['exe_mode'] = "testing";  //執行模式: testing or running
 ?>
