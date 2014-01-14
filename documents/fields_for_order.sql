eabb424785f553e53d189c6f9a5275e7520adb2c
chunshin (cygwin) <l7263626@msn.com>
2014-01-14 05:49:00 +0000

    新增document/sfields_for_order.sql

---
+++ documents/fields_for_order.sql
@@ -0,0 +1,5 @@
+ALTER TABLE  `cht_order` ADD  `TradeNo` VARCHAR( 20 ) NOT NULL ,
+ADD  `RtnCode` INT NOT NULL ,
+ADD  `PayDate` DATETIME NOT NULL ,
+ADD  `AccBank` CHAR(3) NOT NULL ,
+ADD  `AccNo` CHAR( 5 ) NOT NULL ;
