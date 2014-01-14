78eaf203f3811d645c3ac58c700a10a7ac6a1be5
chunshin (cygwin) <l7263626@msn.com>
2014-01-14 01:57:02 +0000

    取號回傳動作

--- card-test3.php
+++ card-test3.php
@@ -9,29 +9,22 @@ include_once("libs/libs-mysql.php");
 $db = new DB($cms_cfg['db_host'],$cms_cfg['db_user'],$cms_cfg['db_password'],$cms_cfg['db_name'],$cms_cfg['tb_prefix']);
 $tpl = new TemplatePower("test3.html");
 $tpl->prepare();
-if($_SESSION['atm_local_result']){
-    if($_SESSION['atm_local_result']['code']){
-        /*初始化payment物件*/
-        $card = new Model_Order_Payment_Allpay_Atm($cms_cfg['vaccount'], $cms_cfg['Hash'],$cms_cfg['exe_mode']);
-        /*解析回傳結果*/
-//        $returnXML = $card->parse_xmldata($_SESSION['atm_local_result']['content']);
-        $returnXML = simplexml_load_string($_SESSION['atm_local_result']['content']);
-//        /*更新訂單*/
-//        $sql = $card->update_order($db,$returnXML);
-        /*輸出回傳結果*/
-        $tpl->gotoBlock("_ROOT");
-        $tpl->assign("UPDATE_ORDER_SQL",$sql);
-        foreach($returnXML->Data->children() as $elm){
-            $tpl->assign("MSG_".strtoupper($elm->getName()),$elm);
-            if($elm->getName()=="RtnCode"){
-                $idx = sprintf("%s",$elm);
-                $tpl->assign("MSG_".strtoupper($elm->getName())."_STR",  Model_Order_Payment_Returncode_Allpay_Atm::$code[$idx]);
-            }
+if($_GET['VAReturn']){
+    /*初始化payment物件*/
+    $card = new Model_Order_Payment_Allpay_Atm($cms_cfg['vaccount'], $cms_cfg['Hash'],$cms_cfg['exe_mode']);
+//    /*解析回傳結果*/
+//    $returnXML = $card->parse_xmldata($_SESSION['atm_local_result']['content']);
+//    /*更新訂單*/
+//    $sql = $card->update_order($db,$returnXML);
+    /*輸出回傳結果*/
+    $tpl->gotoBlock("_ROOT");
+//    $tpl->assign("UPDATE_ORDER_SQL",$sql);
+    foreach($_GET as $key => $value){
+        $tpl->assign("MSG_".strtoupper($key),$value);
+        if($key=="RtnCode"){
+            $tpl->assign("MSG_".strtoupper($key)."_STR",  Model_Order_Payment_Returncode_Allpay_Atm::$code[$value]);
         }
-    }else{
-        
-    }
-    unset($_SESSION['atm_local_result']);
+    }    
 }
 $tpl->printToScreen();
 
