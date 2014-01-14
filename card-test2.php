b2b2d660b07fe6dad5b14b48629d31d8291e9005
chunshin (cygwin) <l7263626@msn.com>
2014-01-14 03:55:03 +0000

    改變引入config.inc.php和vaccount.php的順序

--- card-test2.php
+++ card-test2.php
@@ -1,8 +1,8 @@
 <?php
 require_once "TP/class.TemplatePower.inc.php";
 require_once "class/model/order/payment/allpay/atm.php"; 
-require_once "conf/vaccount.php";
 require_once "conf/config.inc.php";
+require_once "conf/vaccount.php";
 if($_POST){
     $card = new Model_Order_Payment_Allpay_Atm($cms_cfg['vaccount']);
     $bank = $_POST['BankName']?$_POST['BankName']:$cms_cfg['vaccount']['params']['BankName'];
