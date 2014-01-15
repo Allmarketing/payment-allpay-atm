3f0290ff40901a777542fd2a4bb66af34dfb3d75
chunshin (cygwin) <l7263626@msn.com>
2014-01-15 08:59:58 +0000

    更新documents/fields_for_order.sql

--- documents/fields_for_order.sql
+++ documents/fields_for_order.sql
@@ -1,4 +1,8 @@
-ALTER TABLE  `cht_order` ADD  `TradeNo` VARCHAR( 20 ) NOT NULL ,
+ALTER TABLE  `cht_order` 
+ADD  `BankCode` CHAR( 3 ) NOT NULL 
+ADD  `vAccount` VARCHAR( 255 ) NOT NULL 
+ADD  `ExpireDate` DATE NOT NULL 
+ADD  `TradeNo` VARCHAR( 20 ) NOT NULL ,
 ADD  `RtnCode` INT NOT NULL ,
 ADD  `PayDate` DATETIME NOT NULL ,
 ADD  `AccBank` CHAR(3) NOT NULL ,
